660f8f160a7e835a5a4f88340a294d91837fc66c
faudincahyo <fingergreem6@gmail.com>
2023-03-26 14:33:42 +0700

    [FTR] (fix) limit character password

--- src/components/newPassComponent.js
+++ src/components/newPassComponent.js
@@ -1,56 +1,88 @@
-import "../style/newPassStyle.css"
-import { Button, Form } from "react-bootstrap"
-import { useForm } from "react-hook-form"
-import React from "react";
+import "../style/newPassStyle.css";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import { useFormik, Formik } from "formik";
+import * as Yup from "yup";
 
+const schema = Yup.object().shape({
+  password: Yup.string().required().min(6, "at least 6 character"),
+});
 
-function NewPassword(){
+function NewPassword() {
+  {
+    /* <Form.Group as={Col} md="6" controlId="validationFormik03">
+    <Form.Label>City</Form.Label>
+    <Form.Control
+      type="text"
+      placeholder="City"
+      name="city"
+      value={values.city}
+      onChange={handleChange}
+      isInvalid={!!errors.city}
+    />
 
-    const {register, handleSubmit, formState:{errors}} = useForm({
-      defaultValues: {
-        passLimit: ""
-      }
-    });
-    const onSubmit = (data) => {console.log(data)};
+    <Form.Control.Feedback type="invalid">
+      {errors.city}
+    </Form.Control.Feedback>
+  </Form.Group>
 
-
-    return(
+<Button type="submit">Submit form</Button> */
+  }
+  return (
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        password: "",
+      }}
+    >
+      {({ handleSubmit, handleChange, values, errors }) => (
         <div className="newPassForm">
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <div className="newPassContent">
-            <h3 className="mb-4 fw-bold">Buat Password Baru</h3>
+          <Form noValidate onSubmit={handleSubmit}>
+            <div className="newPassContent">
+              <h3 className="mb-4 fw-bold">Create Password</h3>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                className = {'italicText mb-4 ${errors.passLimit && "invalid"}'}
-                type="password"
-                placeholder="*****"
-                {...register("passLimit", {required: true,minLength:{
-                  value: 6,
-                  message: "minimal 6 karakter"
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  className={"italicText mb-4 ${}"}
+                  type="password"
+                  placeholder="*****"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  isInvalid={!!errors.password}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.password}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-                }})}
-              />
-              {errors.passLimit && (<small className="text-danger">Minimal 6 Karakter</small>)}
-            </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Confirmation Password</Form.Label>
+                <Form.Control
+                  className="italicText mb-4"
+                  type="password"
+                  placeholder="*****"
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Konfirmasi Password</Form.Label>
-              <Form.Control
-                className="italicText mb-4"
-                type="password"
-                placeholder="*****"
-              />
-            </Form.Group>
-
-            <Button type="submit" className="w-100 d-grid bg-success mb-2">
-              Login
-            </Button>
-          </div>
-        </Form>
-      </div>
-    )
+              <Button type="submit" className="w-100 d-grid bg-success mb-2">
+                Login
+              </Button>
+            </div>
+          </Form>
+        </div>
+      )}
+    </Formik>
+  );
 }
 
-export default NewPassword
+export default NewPassword;
