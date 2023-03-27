0e649d6d7cc9d67d6e68392d40056a85a2406fce
faudincahyo <fingergreem6@gmail.com>
2023-03-27 09:47:55 +0700

    [FTR] (fix) add validation alert message

--- src/components/loginformComponent.js
+++ src/components/loginformComponent.js
@@ -3,65 +3,84 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import "../style/loginFormStyle.css";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  email: Yup.string().required(),
+  password: Yup.string().required(),
+});
 
 function LoginFormComponent() {
-  const [validated, setValidated] = useState(false);
-
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    setValidated(true);
-  };
 
   return (
-      <div className="loginForm bg-light">
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <div className="loginContent">
-            <h3 className="mb-4 fw-bold">Login</h3>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                className="italicText"
-                type="text"
-                placeholder="Value"
-                minLength={
-                  6
-                }
-                required
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                className="italicText"
-                type="password"
-                placeholder="*****"
-                required
-              />
-            </Form.Group>
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+    >
+      {({ handleSubmit, handleChange, values, errors }) => (
+        <div className="loginForm bg-light">
+          <Form noValidate onSubmit={handleSubmit}>
+            <div className="loginContent">
+              <h3 className="mb-4 fw-bold">Login</h3>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  className="italicText"
+                  type="email"
+                  placeholder="Value"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  isInvalid={!!errors.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  className="italicText"
+                  type="password"
+                  placeholder="*****"
+                  name="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  isInvalid={!!errors.password}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
 
-            <Form.Group className="mb-3">
-              <Form.Check label="Ingat Saya" />
-            </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Check label="Ingat Saya" />
+              </Form.Group>
 
-            <Button type="submit" className="w-100 d-grid bg-success mb-2">
-              Login
-            </Button>
-          </div>
-          <div className="justify-content-end align-items-end d-flex forgot">
-            <Link className="text-success" to={"/confirm"}>
-              Forgot Password
-            </Link>
-          </div>
-        </Form>
-      </div>
+              <Button type="submit" className="w-100 d-grid bg-success mb-2">
+                Login
+              </Button>
+            </div>
+            <div className="justify-content-end align-items-end d-flex forgot">
+              <Link className="text-success" to={"/confirm"}>
+                Forgot Password
+              </Link>
+            </div>
+          </Form>
+        </div>
+      )}
+    </Formik>
   );
 }
 export default LoginFormComponent;
